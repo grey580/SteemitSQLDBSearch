01809050356be85f72bdf552022ed5e5cd80abe2
albert <albert@ALBERT-PC-HOME>
2017-04-12 20:14:11 -0400

    Add project files.

---
+++ SteemitSQLDBSearch/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace SteemitSQLDBSearch
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tbname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dgvResults = new System.Windows.Forms.DataGridView();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.tbProgress = new System.Windows.Forms.TextBox();
+            this.tbTextResult = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tbname
+            // 
+            this.tbname.Location = new System.Drawing.Point(54, 10);
+            this.tbname.Name = "tbname";
+            this.tbname.Size = new System.Drawing.Size(476, 20);
+            this.tbname.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Name";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(609, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dgvResults
+            // 
+            this.dgvResults.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvResults.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvResults.Location = new System.Drawing.Point(12, 247);
+            this.dgvResults.Name = "dgvResults";
+            this.dgvResults.Size = new System.Drawing.Size(906, 168);
+            this.dgvResults.TabIndex = 3;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(16, 70);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(100, 23);
+            this.progressBar1.TabIndex = 4;
+            // 
+            // tbProgress
+            // 
+            this.tbProgress.Location = new System.Drawing.Point(123, 70);
+            this.tbProgress.Name = "tbProgress";
+            this.tbProgress.Size = new System.Drawing.Size(266, 20);
+            this.tbProgress.TabIndex = 5;
+            // 
+            // tbTextResult
+            // 
+            this.tbTextResult.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tbTextResult.Location = new System.Drawing.Point(12, 100);
+            this.tbTextResult.Multiline = true;
+            this.tbTextResult.Name = "tbTextResult";
+            this.tbTextResult.Size = new System.Drawing.Size(906, 132);
+            this.tbTextResult.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(930, 427);
+            this.Controls.Add(this.tbTextResult);
+            this.Controls.Add(this.tbProgress);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.dgvResults);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbname);
+            this.Name = "Form1";
+            this.Text = "Steemit SQL DB Search";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvResults)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tbname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dgvResults;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.TextBox tbProgress;
+        private System.Windows.Forms.TextBox tbTextResult;
+    }
+}
+
---
+++ SteemitSQLDBSearch/Form1.cs
@@ -0,0 +1,117 @@
+﻿//using SteemitSQLDBSearch.model;
+using SteemitSQLDBSearch.model2;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SteemitSQLDBSearch
+{
+    public partial class Form1 : Form
+    {
+        // backgroundworker
+        private BackgroundWorker bw = new BackgroundWorker();
+        public Form1()
+        {
+            InitializeComponent();
+
+            // backgroundworker
+            bw.WorkerReportsProgress = true;
+            bw.WorkerSupportsCancellation = true;
+            bw.DoWork += new DoWorkEventHandler(bw_DoWork);
+            bw.ProgressChanged += new ProgressChangedEventHandler(bw_ProgressChanged);
+            bw.RunWorkerCompleted += new RunWorkerCompletedEventHandler(bw_RunWorkerCompleted);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (bw.IsBusy != true)
+            {
+                bw.RunWorkerAsync();
+            }
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+
+            for (int i = 1; (i <= 10); i++)
+            {
+                if ((worker.CancellationPending == true))
+                {
+                    e.Cancel = true;
+                    break;
+                }
+                else
+                {
+                    // Perform a time consuming operation and report progress.
+                    System.Threading.Thread.Sleep(500);
+                    worker.ReportProgress((i * 10));
+                    // query
+                    /*
+                    DBSteemEntities db = new DBSteemEntities();
+                    var r = db.Accounts.Where(q => q.name == tbname.Text);
+                    var users = r.ToList();
+                    e.Result = r;
+                    //dgvResults.DataSource = users;
+                    foreach(var s in r)
+                    {
+                        //this.tbTextResult.Text += s.name;
+                    }
+                    */
+
+                    steemitDataContext dc = new steemitDataContext();
+                    var r = dc.Accounts.Where(x => x.name == tbname.Text);
+                    List<Account> l = r.ToList<Account>();
+                    e.Result = l;
+
+                        
+                }
+            }
+        }
+        private void bw_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            if ((e.Cancelled == true))
+            {
+                this.tbProgress.Text = "Canceled!";
+            }
+
+            else if (!(e.Error == null))
+            {
+                this.tbProgress.Text = ("Error: " + e.Error.Message);
+            }
+
+            else
+            {
+                this.tbProgress.Text = "Done!";
+
+                List<Account> l = e.Result as List<Account>;
+
+                //dgvResults.DataSource = worker;
+                try
+                {
+                    foreach(var r in l)
+                    {                        
+                        this.tbTextResult.Text +=  r.name + Environment.NewLine;
+                    }
+                    
+                }
+                catch(Exception ex)
+                {
+                    this.tbTextResult.Text = ex.ToString();
+                }
+                
+                
+            }
+        }
+        private void bw_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            this.tbProgress.Text = (e.ProgressPercentage.ToString() + "%");
+        }
+    }
+}
